5039ba639655382e5827bbf0a4d76a61dd4b0a9f
Ben Newman <ben@benjamn.com>
2023-01-21 15:57:26 -0500

    Add GH Actions workflow to run the docker tests.
    Warning: this does not yet build the current contents of the repository
    before running the tests. It just uses the image provided as the first
    argument (benjamn/deno:async-context by default) with whatever version
    is currently published to Docker Hub.

--- docker/tests/docker-run.sh
+++ docker/tests/docker-run.sh
@@ -3,11 +3,9 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
+DENO_IMAGE=${1:-benjamn/deno:async-context}
+echo "Running tests using $DENO_IMAGE"
+
 # Try changing :async-context to :unmodified to observe the tests fail, most
 # immediately because AsyncContext is not defined globally.
-exec docker run \
-    -v $(pwd):/deno \
-    --rm \
-    benjamn/deno:async-context \
-    $@ \
-    test --allow-read *.ts
+exec docker run -v $(pwd):/deno --rm $DENO_IMAGE test --allow-read *.ts
