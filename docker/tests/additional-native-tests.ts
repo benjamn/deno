aed77a929cc12804d669ba422db6257c79bed387
Ben Newman <ben@benjamn.com>
2023-01-22 13:41:20 -0500

    Add test of AsyncContext with native async/await and setTimeout.
    Adapted from my example code in https://github.com/benjamn/deno/pull/2.

---
+++ docker/tests/additional-native-tests.ts
@@ -0,0 +1,33 @@
+// Unlike the tests in adapted-from-proposal.ts, these tests are newly added in
+// the benjamn/deno fork (on the subtext-ideas branch).
+
+import * as assert from "https://deno.land/std@0.173.0/node/assert.ts";
+import { describe, it } from "https://deno.land/std@0.173.0/testing/bdd.ts";
+
+//// TESTS BELOW ARE NEWLY INTRODUCED IN THE benjamn/deno FORK
+
+describe("async via native async/await", () => {
+  it("works after awaited setTimeout result", async () => {
+    const ctx = new AsyncContext<number>();
+    const ctxRunResult = await ctx.run(1234, async () => {
+      assert.strictEqual(ctx.get(), 1234);
+      const setTimeoutResult = await ctx.run(
+        2345,
+        () => new Promise(resolve => {
+          setTimeout(() => resolve(ctx.get()), 20);
+        }),
+      );
+      assert.strictEqual(setTimeoutResult, 2345);
+      assert.strictEqual(ctx.get(), 1234);
+      return "final result";
+    }).then(result => {
+      assert.strictEqual(result, "final result");
+      // The code that generated the Promise has access to the 1234 value
+      // provided to ctx.run above, but consumers of the Promise do not
+      // automatically inherit it.
+      assert.strictEqual(ctx.get(), void 0);
+      return "ctx.run result 👋";
+    });
+    assert.strictEqual(ctxRunResult, "ctx.run result 👋");
+  });
+});
